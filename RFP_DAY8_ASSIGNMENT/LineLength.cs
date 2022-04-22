9c68c80cc756c9e1a34042ae6290829031910700
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-22 18:19:19 +0530

    Add project files.

---
+++ RFP_DAY8_ASSIGNMENT/LineLength.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_DAY8_ASSIGNMENT
+{
+    internal class LineLength
+    {
+        public void lineLengthMethod(int x1,int x2, int y1, int y2)
+        {
+            double length = Math.Sqrt(Math.Pow( (x1-x2),2 ) + Math.Pow( (y1-y2),2 ) ); 
+            Console.WriteLine("Length of line is : {0} units",length);
+
+        }
+    }
+}
---
+++ RFP_DAY8_ASSIGNMENT/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RFP_DAY8_ASSIGNMENT;
+
+Console.WriteLine("\nWelcome to SINGLE LINE LENGTH PROGRAM ...................................................................");
+
+int x1, x2, y1, y2;
+Console.WriteLine("Enter the coordinates value for points ");
+Console.WriteLine("Enter coordinate x for point1");
+x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter coordinate y for point1");
+y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter coordinate x for point2");
+x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter coordinate y for point2");
+y2 = Convert.ToInt32(Console.ReadLine());
+
+LineLength obj1 = new LineLength();
+obj1.lineLengthMethod(x1, x2, y1, y2);
+     
+
